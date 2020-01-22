c9ff44d491f28e770991d4e419a6331a762233aa
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2020-01-22 10:57:19 +0300

    tvOS support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "FACollectionKit",
-    platforms: [.iOS(.v11) ],
+    platforms: [.iOS(.v11),
+                .tvOS(.v13) ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
